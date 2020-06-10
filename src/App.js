fd5f6201e783ac36989e1519ed44a42d301b7c76
suparna3110 <suparna.arora@iaugmentor.com>
2020-06-10 22:11:37 +0530

    Trading Chart for coinbase api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Chart from "./Chart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = theme => ({
+  "chart-container": {
+    height: 400
+  }
+});
+
+class App extends React.Component {
+  state = {
+    lineChartData: {
+      labels: [],
+      datasets: [
+        {
+          type: "line",
+          label: "BTC-USD",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          borderColor: this.props.theme.palette.primary.main,
+          pointBackgroundColor: this.props.theme.palette.secondary.main,
+          pointBorderColor: this.props.theme.palette.secondary.main,
+          borderWidth: "2",
+          lineTension: 0.45,
+          data: []
+        }
+      ]
+    },
+    lineChartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+      tooltips: {
+        enabled: true
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 10
+            }
+          }
+        ]
+      }
+    }
+  };
+
+  componentDidMount() {
+    const subscribe = {
+      type: "subscribe",
+      channels: [
+        {
+          name: "ticker",
+          product_ids: ["BTC-USD"]
+        }
+      ]
+    };
+
+    this.ws = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+    this.ws.onopen = () => {
+      this.ws.send(JSON.stringify(subscribe));
+    };
+
+    this.ws.onmessage = e => {
+      const value = JSON.parse(e.data);
+      if (value.type !== "ticker") {
+        return;
+      }
+
+      const oldBtcDataSet = this.state.lineChartData.datasets[0];
+      const newBtcDataSet = { ...oldBtcDataSet };
+      newBtcDataSet.data.push(value.price);
+
+      const newChartData = {
+        ...this.state.lineChartData,
+        datasets: [newBtcDataSet],
+        labels: this.state.lineChartData.labels.concat(
+          new Date().toLocaleTimeString()
+        )
+      };
+      this.setState({ lineChartData: newChartData });
+    };
+  }
+
+  componentWillUnmount() {
+    this.ws.close();
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes["chart-container"]}>
+        <Chart
+          data={this.state.lineChartData}
+          options={this.state.lineChartOptions}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
